3250ae4886fc15a5ab20aedc42baf8d4c8553bd6
winglight9 <123306733+winglight9@users.noreply.github.com>
2023-04-12 22:50:58 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/xx',
+  socialLink: 'https://twitter.com/winglight',
   seo: {
     keywords: ['Blog', 'Website', '光之翼'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
